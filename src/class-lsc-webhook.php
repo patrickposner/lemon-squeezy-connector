c1cb82e0113bb9ff09ec0af722c9a88f55ada9c0
Patrick Posner <hello@patrickposner.dev>
2022-02-22 15:20:22 +0100

    modified webhook handling

--- src/class-lsc-webhook.php
+++ src/class-lsc-webhook.php
@@ -38,14 +38,8 @@ class Webhook {
 	 * Handles the HTTP Request sent from order_created event in Lemon Squeezy.
 	 */
 	public function register_customer() {
-
-		// Check if credentials are set.
-		if ( ! defined( 'LS_SECRET' ) && false === LS_SECRET ) {
-			return;
-		}
-
 		// Validate it's coming from Lemon Squeezy.
-		$secret    = LS_SECRET;
+		$secret    = 'ls_order_changed'; // add webhook secret.
 		$payload   = file_get_contents( 'php://input' );
 		$hash      = hash_hmac( 'sha256', $payload, $secret );
 		$signature = $_SERVER['HTTP_X_SIGNATURE'] ?? null;
@@ -70,27 +64,30 @@ class Webhook {
 		$users_exists = email_exists( $customer['user_email'] );
 
 		if ( $users_exists ) {
-			if ( 'subscription_cancelled' === $customer['event'] ) {
-				error_log( 'Subscription for user with E-Mail ' . $customer['user_email'] . ' was cancelled' );
-			} elseif ( 'subscription_created' === $customer['event'] ) {
-				error_log( 'Subscription for user with E-Mail ' . $customer['user_email'] . ' was created' );
-			} elseif ( 'subscription_updated' === $customer['event'] ) {
-				error_log( 'Subscription for user with E-Mail ' . $customer['user_email'] . ' was updated' );
-			}
-
-			// Update and add meta field with number of websites (and currently used ones).
+			// Only update metadata.
+			update_user_meta( $user_id, 'purchase-date', sanitize_text_field( $customer['created_at'] ) );
+			update_user_meta( $user_id, 'purchase-product', sanitize_text_field( $customer['variant'] ) );
 		} else {
-			$password = wp_generate_password( 12, true );
-			$user_id  = wp_create_user( $customer['full_name'], $password, $customer['user_email'] );
+			// Check that a new order was created.
+			if ( 'order_created' === $customer['event'] ) {
+				$password = wp_generate_password( 12, true );
+				$user_id  = wp_create_user( $customer['full_name'], $password, $customer['user_email'] );
 
-			// Set customer role.
-			$user = new WP_User( $user_id );
-			$user->set_role( 'customer' );
+				// Update meta data.
+				update_user_meta( $user_id, 'purchase-date', sanitize_text_field( $customer['created_at'] ) );
+				update_user_meta( $user_id, 'purchase-product', sanitize_text_field( $customer['variant'] ) );
 
-			// Update and add meta field with number of websites (and currently used ones).
+				// Send e-mail to the user.
+				wp_new_user_notification( $user_id, null, 'both' );
 
-			// Send e-mail to the user.
-			wp_new_user_notification( $user_id, null, 'both' );
+				// Send post request with data to URL (optional).
+				$args = array(
+					'e-mail'    => $customer['user_email'],
+					'full_name' => $customer['full_name'],
+				);
+
+				//wp_remote_post( 'https://sample-url.com' ), $args );
+			}
 		}
 	}
 }
